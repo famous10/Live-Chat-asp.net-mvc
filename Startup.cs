1878077f92fd1cc9038af712f61b60ee10b0ea97
famous <famous0814@gmail.com>
2021-03-05 23:35:28 +0100

    first commit

---
+++ Startup.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebLiveChat.Startup))]
+namespace WebLiveChat
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.MapSignalR();
+
+        }
+    }
+}
